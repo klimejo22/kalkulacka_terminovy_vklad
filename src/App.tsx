1877b15a72e7cb61de97051c526ce45fc4c1ddf4
klimejo22 <klimejo22@sps-prosek.cz>
2025-11-11 11:20:23 +0100

    Pridani bank

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const banks = {
+    "Moneta": 3.4,
+    "Česká spořitelna": 2.25,
+    "Komerční banka": 3,
+    "Air Bank" : 3.2
+  }
+
   return (
     <>
-      
+      <h1>Kalkulačka termínovaných vkladů</h1>
     </>
   )
 }
